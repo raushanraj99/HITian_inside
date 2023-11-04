f0666d9164b890b8957250ea823c209b920f6313
raushanraj99 <raushanraj9950@gmail.com>
2023-11-04 15:26:28 +0530

    merchandise modified

--- src/pages/Merchandise/Merchandise.jsx
+++ src/pages/Merchandise/Merchandise.jsx
@@ -1,25 +1,38 @@
 import React from 'react'
-import "./Merchandise.css";
+// import "./Merchandise.css";
+
 import tshirtf from "../../assets/images/t-shirt-front.jpg";
 import tshirtb from "../../assets/images/t-shirt-back.jpg";
 import qr from "../../assets/images/QR.png";
 
 function Merchandise() {
   return (
+    <section className='bg-[#650808] pt-5 pb-10'>
 
-    <>
-      <div className='merch-home'>
-        <div className='merch-display'>
-          <h1 style={{overflow:"hidden",fontSize:"3rem",color:"#650808",fontWeight:"700"}}>The HITian Inside</h1>
-          <div className="merch">
-            <img src={tshirtf} alt="front tshirt" />
-            <img src={tshirtb} alt="back tshirt" />
+    <h1 style={{fontSize:"3rem",color:"#f5b5b5",fontWeight:"700"}}>Attire of 2023</h1>
+    <div className='merch-home bg-[#650808] mt-10 flex  justify-center'>
+        <div className='bg-[#f5b5b5] w-[65vw] h-[95vw] md:w-[44vw] md:h-[40vw] rounded-md'>
+
+          <div className="merch flex flex-row justify-center items-center p-5 ">
+            <img src={tshirtf} alt="front tshirt" className='w-[28vw] md:w-[20vw]' />
+            <img src={tshirtb} alt="back tshirt" className='w-[28vw] md:w-[20vw]' />
           </div>
-          <img src={qr} alt="QR" className='qr'/>
+          <div className="flex flex-col md:flex-row justify-center">
+          <img src={qr} alt="QR" className=' w-[22vw] md:w-[10vw] ms-20 md:ms-5'/>
+          <div className="flex flex-col justify-center items-center font-bold ps-2 pe-2 text-[.8rem] md:text-[1rem]">
+          <p>Scan the Qr to Order, contact <span className='bg-[#650808] md:bg-[#650808]'>👇</span> for queries</p>
+          <p>Raushan Raj : XXXXXXXXXX</p>
+          <p>Raushan Raj : XXXXXXXXXX</p>
+
+          </div>
+
+          </div>
 
         </div>
+        
       </div>
-    </>
+      
+    </section>
   )
 }
 
